2dbeb241b0aa6eac3ddbda81960557b8ad289aee
Dylan-Santiago-Villarroel-Celi <150629720+Dylan-Santiago-Villarroel-Celi@users.noreply.github.com>
2024-01-25 23:17:56 -0500

    Header y Footer de la web

--- Front/index.php
+++ Front/index.php
@@ -20,7 +20,7 @@ $habitaciones = $resultado->fetch_all(MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda Online</title>
+    <title>Copo De Nieve</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
