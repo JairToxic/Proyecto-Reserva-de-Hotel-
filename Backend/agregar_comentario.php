598b28d335b6407c918bdfb908e206b6be61fe62
cmGitHub192 <cmGitHub192@gmail.com>
2024-01-28 23:41:32 -0500

    agregar_archivo_agregar_comentario.php

---
+++ Backend/agregar_comentario.php
@@ -0,0 +1,36 @@
+<?php
+require_once ("dbconnect.php");
+$commentId = isset($_POST['comment_id']) ? $_POST['comment_id'] : "";
+$comment = isset($_POST['comment']) ? $_POST['comment'] : "";
+$commentSenderName = isset($_POST['name']) ? $_POST['name'] : "";
+$date = date('Y-m-d H:i:s');
+
+$query = "INSERT INTO tbl_comentarios(parent_id,comentarios,comentario_nombre,fecha) VALUES (?,?,?,?)";
+
+$sql_stmt = $conn->prepare($query);
+
+$param_type = "dsss";
+$param_value_array = array(
+    $commentId,
+    $comment,
+    $commentSenderName,
+    $date
+);
+$param_value_reference[] = & $param_type;
+for ($i = 0; $i < count($param_value_array); $i ++) {
+    $param_value_reference[] = & $param_value_array[$i];
+}
+call_user_func_array(array(
+    $sql_stmt,
+    'bind_param'
+), $param_value_reference);
+
+$sql_stmt->execute();
+
+if ($sql_stmt->error) {
+    echo "Error al ejecutar la consulta: " . $sql_stmt->error;
+} else {
+    echo "Comentario agregado con éxito!";
+}
+
+?>
