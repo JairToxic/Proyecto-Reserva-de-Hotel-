c194fe77ec8c5efccb8f18841ed7deedc8d817a3
cmGitHub192 <cmGitHub192@gmail.com>
2024-02-06 00:04:33 -0500

    cambios_en_archivos_y_añadir_imagenes

--- Front/buscar/buscar_habitaciones.php
+++ Front/buscar/buscar_habitaciones.php
@@ -13,13 +13,13 @@ $habitacionesDisponibles = array();
 // Verificar si se envió el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtener datos del formulario
-    $checkin = $_POST['checkin'];
-    $checkout = $_POST['checkout'];
-    $capacidad = $_POST['capacidad'];
+    $checkin = $_POST['checkIn'];
+    $checkout = $_POST['checkOut'];
+    $capacidad = $_POST['numberOfPeople'];
 
     // Consulta SQL para obtener habitaciones disponibles
     $query = "SELECT * FROM habitaciones
-              WHERE CAPACIDAD >= $capacidad
+              WHERE CAPACIDAD = $capacidad
               AND ID_HABITACION NOT IN (
                   SELECT hr.ID_HABITACION FROM habitacion_reserva hr
                   JOIN reserva r ON hr.ID_RESERVA = r.ID_RESERVA
@@ -37,20 +37,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Cerrar conexión
 $conexion->close();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Habitaciones Disponibles</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-        }
 
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+<head>
+     <!-- Site Title-->
+    <title>Buscar_Habitaciones</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <link rel="icon" href="../site/images/favicon.ico" type="../image/x-icon">
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="../site/text/css" href="//fonts.googleapis.com/css?family=Lato:400,700,400italic%7CPoppins:300,400,500,700">
+    <link rel="stylesheet" href="../site/css/bootstrap.css">
+    <link rel="stylesheet" href="../site/css/style.css">
+    <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
+    <style>
+ 
         .productos-container {
             display: flex;
             flex-wrap: wrap;
@@ -69,7 +73,7 @@ $conexion->close();
         }
 
         .producto-imagen {
-            width: 100%;
+            width: 150px;
             height: 150px;
             object-fit: cover;
             border-bottom: 1px solid #ddd;
@@ -104,16 +108,115 @@ $conexion->close();
     </style>
 </head>
 <body>
-
-
-
-
+<div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="../site/images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <!-- Page-->
+    <div class="text-center page"><a class="banner banner-top" href="https://www.templatemonster.com/website-templates/monstroid2.html" target="_blank"><img src="../site/images/monstroid.jpg" alt="" height="0"></a>
+      <!-- Page preloader
+      <div class="page-loader">
+        <div>
+          <div class="page-loader-body">
+            <div class="loader">
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="window"></div>
+              <div class="door"></div>
+              <div class="hotel-sign"><span>H</span><span>O</span><span>T</span><span>E</span><span>L</span></div>
+                <div class="hotel-sign1"></span><span></span><span>C</span><span>O</span>
+                    <span>P</span><span>O</span></div>
+            </div>
+          </div>
+        </div>
+      </div>-->
+      <!-- Page Header-->
+      <header class="page-header" style="padding-bottom: 24px">
+        <!-- RD Navbar-->
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar rd-navbar-default-with-top-panel" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fullwidth" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fullwidth" data-lg-device-layout="rd-navbar-fullwidth" data-md-stick-up-offset="90px" data-lg-stick-up-offset="150px" data-stick-up="true" data-sm-stick-up="true" data-md-stick-up="true" data-lg-stick-up="true">
+            <div class="rd-navbar-top-panel rd-navbar-collapse">
+              <div class="rd-navbar-top-panel-inner">
+                <div class="left-side">
+                  <div class="group"><span class="text-italic">Siguenos en Redes Sociales:</span>
+                    <ul class="list-inline">
+                      <li><a class="icon icon-sm icon-secondary-5 fa fa-instagram" href="#"></a></li>
+                      <li><a class="icon icon-sm icon-secondary-5 fa fa-facebook" href="#"></a></li>
+                      <li><a class="icon icon-sm icon-secondary-5 fa fa-twitter" href="#"></a></li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="center-side">
+                  <!-- RD Navbar Brand-->
+                  <div class="rd-navbar-brand fullwidth-brand"><a class="brand-name" href="../site/index.html"><img src="../site/imagenes/logo.jpeg" alt="" width="214" height="28"/></a></div>
+                </div>
+                <div class="right-side">
+                  <!-- Contact Info-->
+                  <div class="contact-info">
+                    <div class="unit unit-middle unit-horizontal unit-spacing-xs">
+                      <div class="unit__left"><span class="icon icon-primary text-middle mdi mdi-phone"></span></div>
+                      <div class="unit__body"><a class="text-middle" href="tel:#">+ (593) 987297703</a></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="rd-navbar-inner">
+              <!-- RD Navbar Panel-->
+              <div class="rd-navbar-panel">
+                <!-- RD Navbar Toggle-->
+                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                <!-- RD Navbar collapse toggle-->
+                <button class="rd-navbar-collapse-toggle" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></button>
+                <!-- RD Navbar Brand-->
+                <div class="rd-navbar-brand mobile-brand"><a class="brand-name" href="../site/index.html"><img src="../site/images/logo-default-314x48.png" alt="" width="314" height="48"/></a></div>
+              </div>
+              <div class="rd-navbar-aside-right">
+                <div class="rd-navbar-nav-wrap">
+                  <div class="rd-navbar-nav-scroll-holder">
+                    <ul class="rd-navbar-nav">
+                      <li class="active"><a href="../site/index.html">Hotel</a>
+                      </li>
+                      <li><a href="../site/about-us.html">Sobre Nosotros</a>
+                      </li>
+                      <li><a href="../site/contacts.html">Contactanos</a>
+                      </li>
+                      <li><a href="../site/habitaciones.html">Habitaciones</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+    
     <!-- Mostrar todas las habitaciones -->
 <div class="productos-container">
     <?php foreach ($habitacionesDisponibles as $habitacion) : ?>
         <div class="producto">
             <!-- Asume que todas las imágenes son .jpg. Modifica según tus necesidades -->
-            <img src="imagenes/<?php echo $habitacion['ID_HABITACION']; ?>.jpg" alt="Imagen de la habitación" class="producto-imagen">
+            <img src="../imagenes/<?php echo $habitacion['ID_HABITACION']; ?>.jpg" alt="Imagen de la habitación" class="producto-imagen">
             <div class="contenido">
                 <h2 class="producto-titulo"><?php echo $habitacion['TIPO']; ?></h2>
                 <p class="card-title">👥<?php echo $habitacion['CAPACIDAD']; ?> personas</p>
@@ -128,6 +231,78 @@ $conexion->close();
         </div>
     <?php endforeach; ?>
 </div>
+
+ <!-- Page Footer-->
+ <footer class="page-footer text-left text-sm-left">
+        <div class="shell-wide">
+        
+          <div class="page-footer-minimal">
+            <div class="shell-wide">
+              <div class="range range-50">
+                <div class="cell-sm-6 cell-md-3 cell-lg-4 wow fadeInUp" data-wow-delay=".1s">
+                  <div class="page-footer-minimal-inner">
+                    <h4>Horario de apertura</h4>
+                    <ul class="list-unstyled">
+                      <li>
+                        <div class="group-xs"> 
+                          <div>
+                            <dl class="list-desc">
+                              <dt>Días laborables: </dt>
+                              <dd class="text-gray-darker"><span>8:00–20:00</span></dd>
+                            </dl>
+                          </div>
+                          <div>
+                            <dl class="list-desc">
+                              <dt>Fines de semana: </dt>
+                              <dd class="text-gray-darker"><span>9:00–18:00</span></dd>
+                            </dl>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <p class="rights"><span>&copy;&nbsp;</span><span>2024</span><span>&nbsp;</span><span class="copyright-year"></span>Hotel Copo de Nieve. Reservados todos los derechos. Diseñado por Jair Sanchez <a href="https://www.templatemonster.com">Jair</a></p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="cell-sm-6 cell-md-5 cell-lg-4 wow fadeInUp" data-wow-delay=".2s">
+                  <div class="page-footer-minimal-inner">
+                    <h4>DIRECCIÓN</h4>
+                    <ul class="list-unstyled">
+                      <li>
+                        <dl class="list-desc">
+                          <dt><span class="icon icon-sm mdi mdi-map-marker"></span></dt>
+                          <dd><a class="link link-gray-darker" href="#">Quito,12 de Octubre </a></dd>
+                        </dl>
+                      </li>
+                      <li>
+                        <dl class="list-desc">
+                          <dt><span class="icon icon-sm mdi mdi-phone"></span></dt>
+                          <dd class="text-gray-darker">Llamanos: <a class="link link-gray-darker" href="tel:#">+ (593) 987297703</a>
+                          </dd>
+                        </dl>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="cell-sm-8 cell-md-4 wow fadeInUp" data-wow-delay=".3s">
+                  <div class="page-footer-minimal-inner-subscribe">
+                    <h4>Suscríbase a nuestro boletín</h4>
+                    <!-- RD Mailform-->
+                    <form class="rd-mailform rd-mailform-inline form-center" data-form-output="form-output-global" data-form-type="subscribe" method="post" action="">
+                      <div class="form-wrap">
+                        <input class="form-input" id="subscribe-email" type="email" name="email" data-constraints="@Email @Required">
+                        <label class="form-label" for="subscribe-email">Introduce tu correo electrónico</label>
+                      </div>
+                      <button class="button button-primary-2 button-effect-ujarak button-square" type="submit"><span>Subscribe</span></button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
 
 </body>
 </html>
