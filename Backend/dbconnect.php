25c2d62c437f72f7dc035d5ef1452cf6327d7ddb
cmGitHub192 <cmGitHub192@gmail.com>
2024-02-12 20:49:53 -0500

    actualizacion_de_pagina_comentarios

--- Backend/dbconnect.php
+++ Backend/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-   $conn = mysqli_connect("localhost", "root", "", "hotel");
+   $conn = mysqli_connect("localhost", "root", "", "hotelactualizado");
    // Establecer el conjunto de caracteres a utf8mb4
    $conn->set_charset('utf8mb4');
 ?>
